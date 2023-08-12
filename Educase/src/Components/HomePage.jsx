2cf26ad45d9297aa92b03e4d4537ae399ee624a2
yogeshnishad007 <yogiatoy27518@gmail.com>
2023-08-12 19:40:39 +0530

    some change

--- Educase/src/Components/HomePage.jsx
+++ Educase/src/Components/HomePage.jsx
@@ -1,41 +1,27 @@
-
-import { Link } from "react-router-dom"
-import "../Style/Home.css"
+import { Link } from "react-router-dom";
+import "../Style/Home.css";
 
 const HomePage = () => {
   return (
     <div className="home-conatiner">
+      <div className="parent-container">
+        <div className="child">
+          <h2>Welcome to PopX</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
 
-             <div className="parent-container">
+          <div className="link">
+            <Link to="/signup">
+              <button className="btn purple">Create Account</button>
+            </Link>
 
-                    
-                    <div  className="child">
-
-                         <h2>Welcome to PopX</h2>
-                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                             </p>
-            
-                    <div  className="link">
-
-                       <Link to="/signup">
-                          <button className="btn purple">Create Account</button>
-                        </Link>
-
-                        <Link to="/login">
-                           <button className="btn grey">Already Registered?Login</button>
-                        </Link>
-
-                    </div>
-                    
-
-                    </div>
-            
-
-             </div>
-
-           
+            <Link to="/login">
+              <button className="btn grey">Already Registered?Login</button>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
--- Educase/src/Components/Login.jsx
+++ Educase/src/Components/Login.jsx
@@ -1,10 +1,31 @@
 
-
+import "../Style/Login.css"
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div>
+      <div className="parent-container">
+        <div className="child">
+          <h2>Signin to your PopX account</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+             
+          <div className="input-wrapper">
+              <label className="label-1"  >Email</label>
+              <input type="text" placeholder="Enter Email"/>
 
-export default Login
+              <label className="label-2" >Password</label>
+              <input type="text" placeholder="Enter Password"/>
+          </div>
+
+          <div className="link">
+           
+              <button className="btn purple">Login</button>
+  
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
