b68aaa6a984679342b8fbe2880b1b3e1ae1f0ae3
yogeshnishad007 <yogiatoy27518@gmail.com>
2023-08-13 17:37:42 +0530

    profile page add

--- Educase/src/Components/Login.jsx
+++ Educase/src/Components/Login.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import "../Style/Login.css"
+import "../Style/Login.css";
 
 const Login = () => {
   return (
@@ -8,22 +8,19 @@ const Login = () => {
         <div className="child">
           <h2>Signin to your PopX account</h2>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-             
-          <div className="input-wrapper">
-              <label className="label-1"  >Email</label>
-              <input type="text" placeholder="Enter Email"/>
 
-              <label className="label-2" >Password</label>
-              <input type="text" placeholder="Enter Password"/>
+          <div className="input-wrapper">
+            <label className="label-1">Email</label>
+            <input type="text" placeholder="Enter Email" />
+
+            <label className="label-2">Password</label>
+            <input type="text" placeholder="Enter Password" />
           </div>
 
           <div className="link">
-
-                    <Link to="/profile">
-                        <button className="btn purple">Login</button> 
-                    </Link>
-              
-  
+            <Link to="/profile">
+              <button className="btn purple">Login</button>
+            </Link>
           </div>
         </div>
       </div>
--- Educase/src/Components/Profile.jsx
+++ Educase/src/Components/Profile.jsx
@@ -1,9 +1,35 @@
-
-
+import "../Style/Profile.css";
 const Profile = () => {
   return (
-    <div>Profile</div>
-  )
-}
+    <div>
+      <div className="parent-container">
+        <div className="child">
+          <h4 className="heading">Account Setting</h4>
 
-export default Profile
+          <div className="info-box">
+            <div className="image-box">
+              <img
+                src="https://w7.pngwing.com/pngs/613/636/png-transparent-computer-icons-user-profile-male-avatar-avatar-heroes-logo-black-thumbnail.png"
+                alt=""
+              />
+            </div>
+
+            <div>
+              <p className="heading">Yogesh</p>
+              <p>yogi27518@gmail.com</p>
+            </div>
+          </div>
+
+          <div>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae
+              tenetur temporibus ullam, et qui voluptatibus laboriosam{" "}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
