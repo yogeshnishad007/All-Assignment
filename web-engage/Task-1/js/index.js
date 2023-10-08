ad8da7287a172fd1ec166a1f111dc78f35ea2e57
yogeshnishad007 <yogiatoy27518@gmail.com>
2023-10-08 23:53:47 +0530

    complete task-1

--- web-engage/Task-1/js/index.js
+++ web-engage/Task-1/js/index.js
@@ -4,15 +4,30 @@ document.getElementById('talk-to-us').addEventListener('click', function() {
     const countryCode = document.getElementById('country-code').value;
     const mobile = document.getElementById('mobile').value;
     const errorMsg = document.querySelector('.error-msg')
-    
-    console.log("len",mobile.length)
-    // console.log(email,mobile)
+    const  mail = document.getElementById("email").value;
 
-    errorMsg.textContent = ""; 
+
+    
+     errorMsg.textContent = ""; 
+     
+
+    console.log("len",mobile.length)
+    
+
+        // var regx = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/
+        var regx = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+\.([a-z]{2,})(\.[a-z]{2,})?$/;
+
+        if(!regx.test(mail)){
+
+            const err = "Sorry! Incorrect Email ID";
+            errorMsg.textContent = err;
+            return
+        }
 
     if (!email && !mobile) {
         const err = "Please fill out all fields. ";
         errorMsg.textContent = err;
+        return
     }
 
     if (!email) {
@@ -23,13 +38,15 @@ document.getElementById('talk-to-us').addEventListener('click', function() {
     if (!countryCode) {
         const err = "Please select a country code. ";
         errorMsg.textContent = err;
+        return
     }
 
+  
     if(mobile.length < 10) {
         const err = "Mobile Number should be at least 10 digits.";
         errorMsg.textContent = err;
+        return
     } 
-    
    
 
     if (email && countryCode && mobile.length >= 10) {
